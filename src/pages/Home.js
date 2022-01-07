04196623fd38c44031fea8380175180f916f90af
Hector Alvarez-Toledo <hect16@gmail.com>
2022-01-07 17:01:43 -0600

    grid changes, home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,46 +2,54 @@ import React , {useState, useEffect , useContext} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import Programming from '../assets/homepage.jpeg'
+import backend from '../assets/backend.jpeg'
+import database from '../assets/database.png'
+import frontend from '../assets/frontend.png'
+import language from '../assets/languages.jpeg'
+import cloudtech from '../assets/cloudtech.jpeg'
+import agile from '../assets/agile.jpeg'
+
+
 function Home() {
 
     const skills = [
         {
             title : "Programming Languages",
             detail: "Python, JavaScript/TypeScript , C#, HTML , CSS" ,
-            image : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zdnet.com%2Feducation%2Fcomputers-tech%2Feasiest-programming-languages-to-learn%2F&psig=AOvVaw3P1IzZ9ykmTpKr4TJZ_Z7i&ust=1641522801068000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjLvI6LnPUCFQAAAAAdAAAAABAD"
+            image : language
         }, 
         {
             title : "Front End Development",
             detail: "Frameworks: ReactJS/ReactNative, Angular (Ionic), Styling:  SCSS , Tailwind" ,
-            image : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mindinventory.com%2Fblog%2Ftop-front-end-development-tools%2F&psig=AOvVaw2roSlHzlaJ_RrrMYzqQU4T&ust=1641522835048000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjE556LnPUCFQAAAAAdAAAAABAI"
+            image : frontend
         },
         {
             title : "Back End Development",
             detail: "NodeJS, Express" ,
-            image : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcodecondo.com%2Fawesome-facts-that-will-make-you-be-a-back-end-developer%2F&psig=AOvVaw2jaB_snhKAudex84LWTx5o&ust=1641522868039000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDEqq6LnPUCFQAAAAAdAAAAABAD"
+            image : backend
         }, 
         {
             title : "Databases",
             detail: "MongoDB , DynoDB , SQL , PostGres" ,
-            image : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.toptal.com%2Fdatabase%2Fdatabase-design-bad-practices&psig=AOvVaw0xBWYLl-tCwyTIRtbbbg7X&ust=1641522890318000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPD4mbqLnPUCFQAAAAAdAAAAABAD"
+            image : database
         }, 
         {
             title : "Cloud Technologies",
             detail: "AWS, Terraform , Jenkins , CI/CD , Docker, Kubernetes , Linux" ,
-            image : ""   
+            image : cloudtech  
         },
         {
             title : "Team Work and Leadership",
             detail: "Git, Github, Agile, Scrum, Kanban" ,
-            image : ""
+            image : agile
         }
     ]
 
     return(
         <section className="snap-y snap-mandatory overflow-scroll h-screen w-screen  ">
 
-            <section className="h-screen w-screen bg-amber-50 p-20 snap-start flex place-items-center justify-center">
-                <div className="bg-purple-200 rounded-3xl flex justify-center place-items-center ">
+            <section className="h-screen w-screen bg-amber-50 p-20 snap-start flex place-items-center justify-center ">
+                <div className="h-4/5 w-4/5 bg-purple-200 rounded-3xl flex justify-center place-items-center ">
                     <div className="flex-1 m-4 flex flex-col "> 
                         <h1 className="text-4xl font-serif justify-left">Welcome </h1>
                         <h2>Meet your new Software Engineer/Developer</h2>
@@ -57,20 +65,20 @@ function Home() {
             </section>
         
 
-            <section className="h-screen w-screen font-sansbg-red-300 p-20 snap-start flex place-items-center justify-center" id="skills">
-                <div className="h-4/5 w-4/5 flex flex-col bg-sky-200">
+            <section className="h-screen w-screen font-sansbg-red-300 p-20 snap-start flex place-items-center justify-center overflow-scroll" id="skills">
+                <div className="h-full w-full flex flex-col bg-sky-200 place-items-center justify-center">
                     <div className="font-sans text-4xl "> Skills </div>
-                    <div className="grid grid-cols-3"> 
+                    <div className="h-full w-full grid grid-cols-3 grid-gap-5 p-5" > 
                     {
                     skills.map( (item)=> {
                         return (
-                            <div className="flex flex-col place-items-center justify-center bg-yellow-200 p-2 m-5 rounded-3xl" id="card">
-                                <div className="flex-1" >
-                                    <h2>{item.title}</h2>
-                                    <h2>{item.detail}</h2>
+                            <div className="h-5/6 w-5/6 flex flex-col grow flex-wrap place-items-center justify-center bg-yellow-200 p-2 m-3 rounded-3xl  overflow-y-hidden hover:scale-125" id="card">
+                                <div className="flex-1 h-full w-full bg-red-200 overflow-y-hidden flex flex-col justify-center place-items-center" >
+                                    <h2 className=" font-serif text-2xl">{item.title}</h2>
+                                    <h2 className= "font-serif text-base ">{item.detail}</h2>
                                 </div>
-                                <div className="flex-1">
-                                    <img src={item.image} />
+                                <div className="flex-1 h-full w-full bg-green-400 overflow-y-hidden ">
+                                     <img className="object-cover" src={item.image} /> 
                                 </div>
                             </div>
                         )
