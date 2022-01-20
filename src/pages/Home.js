23fbabf5cddfad1e4b2f2859f1c937c0c2b00ecf
Hector Alvarez-Toledo <hect16@gmail.com>
2022-01-20 12:17:21 -0600

    changes to website design color scheme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,13 +48,18 @@ function Home() {
     return(
         <section className="snap-y snap-mandatory overflow-scroll h-screen w-screen  ">
 
-            <section className="h-screen w-screen bg-amber-50 p-20 snap-start flex place-items-center justify-center ">
-                <div className="h-4/5 w-4/5 bg-purple-200 rounded-3xl flex justify-center place-items-center ">
+            <section className="h-screen w-screen bg-gradient-to-b from-indigo-900 to-neutral-900 p-20 snap-start flex place-items-center justify-center text-white">
+                <div className="h-4/5 w-4/5 rounded-3xl flex justify-center place-items-center ">
                     <div className="flex-1 m-4 flex flex-col "> 
                         <h1 className="text-4xl font-serif justify-left">Welcome </h1>
                         <h2 className='m-3'>Meet your new Software Engineer/Developer</h2>
                         <div className='m-3' title="Icons">
                             <button> <FontAwesomeIcon icon={faFacebookF} /></button>
+                        </div>
+                        
+                        <div> 
+                            <button className=" text-white border-2  p-3 m-3 border-slate-100 hover:text-black hover:bg-slate-100 hover:border-black" > Learn More </button>
+                            <button className="text-white border-2  p-3 m-3 border-slate-100"> Check out Projects </button>
                         </div>
                     </div>
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,53 +1,68 @@
 import React , {useState, useEffect , useContext} from 'react' 
+import weight from '../assets/weightloss.jpeg'
+import crypto from '../assets/crypto.jpeg'
 
 
 function Projects() {
 
     const projects = [
         {
-            name : "Project X",
-            image : "image 1"
+            name : "University Capstone Project",
+            image : "https://www.boomfit.com/img/leoblog/b/1/22/lg-b-artigo%20apps.jpg", 
+            link: "https://github.com/clouds16/capstone-mobileapp",
+            description : "Fitness Application, Built to keep track of Gym Statistics and have a virtual Trainer. Built With React-Native, NodeJS , Express, MongoDB, Deployed using AWS Ubuntu server."
         }, 
         {
-            name : "Project Y",
-            image : "image 2"
+            name : "Weight Watching App",
+            image : "image 2", 
+            link: "require('../assets/weightloss.jpeg')", 
+            description: "Application for Weight documentation over time. Built in Ionic Angular"
         }, 
         {
-            name : "Project Z",
-            image : "image 3"
+            name : "Pizza Ordering System",
+            image : "https://saboryestilo.com.mx/wp-content/uploads/elementor/thumbs/masa-para-pizza-3-1-os3aa3ck56334eoe88d8hkem59xt1jziomikxlzx34.jpg", 
+            link: "https://github.com/clouds16/heroku-pizza-app",
+            description: "Application Built For WebApps Class. Pizza ordering system such as Dominoes. Application built using Flask, CSS , HTML, JS. Purpose of the Project was to display knowledge of how to properly use OOP and create a website with proprietary class based system."
+        },
+      
+        {
+            name : "Cyrpto Mock Purchasing ",
+            image : "image 3", 
+            link: "https://image.cnbcfm.com/api/v1/image/106984655-1638720074358-gettyimages-1294303237-01_jan_01_005.jpeg?v=1641825196&w=929&h=523",
+            description: "Crypto Purchasing and Selling Application.",
         },
         {
-            name : "Project X",
-            image : "image 1"
-        }, 
-        {
-            name : "Project Y",
-            image : "image 2"
-        }, 
-        {
-            name : "Project Z",
-            image : "image 3"
+            name : "Ionic Native Weather App",
+            image : "image 3", 
+            link: "https://assets.thehansindia.com/h-upload/2021/08/17/1102463-pain.webp",
+            description: "Wether and Location Application using React Native, NodeJS backend",
         },
         {
-            name : "Project Z",
-            image : "image 3"
-        },
-        {
-            name : "Project Z",
-            image : "image 3"
+            name : "SQL Database",
+            image : "image 3", 
+            link: "https://dv-website.s3.amazonaws.com/uploads/2020/11/kf_graphdbupdates_011221.jpg",
+            description: "This is a SQL Database created for a potential Game server. The Database was made to show proper use of DataBase Design using Private/Foreight key principles, Table relationships (one to one, many to one)"
         }
     ]
 
 
     return(
-        <section className="h-screen w-screen bg-red-100 flex justify-center place-items-center ">
-            <div className="h-4/5 w-5/6 flex justify-center place-items-center bg-sky-400" >
-                <div className="grid grid-cols-4 gap-5"> 
+        <section className="h-screen w-screen bg-gradient-to-b from-indigo-900 to-neutral-900 flex justify-center place-items-center ">
+            <div className="h-4/5 w-5/6 flex justify-center place-items-center " >
+                <div className="grid grid-cols-3 gap-5"> 
                 { projects.map( (item) => {
                     return (
-                    <div className="flex flex-col h-60 w-60 items-center bg-sky-300 m-4 rounded-2xl">
-                        <h2 className="flex-1">  {item.name} </h2>
-                        <div className="flex-1 "> {item.image} </div>
+
+                    <div className="flex flex-col h-4/5 w-4/5 items-center bg-gray-100 opacity-50 m-4 rounded-2xl ">
+                        <img src={item.image} className="fill backdrop-blur-xl relative"></img>
+                        <div className="flex-5 absolute"> 
+                            <h2 className="">  {item.name} </h2>
+                            <h3> {item.description} </h3>
+                            
+                        </div>
+                        <button className="flex-1 bg-emerald-500 p-3 m-2 rounded-xl border-2 "> <a href={item.link}> Open Project on GitHub</a></button>
+                        
+                    
                     </div> 
                     )})
                 }
